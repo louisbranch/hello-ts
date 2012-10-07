51f5bdfe5db461f9a64bd77564646de64a2745a2
Luiz Branco <me@luizbranco.com>
2012-10-07 11:25:42 -0300

    Create respond function

--- server.ts
+++ server.ts
@@ -3,10 +3,12 @@
 import http = module("http");
 import response = module("./response");
 
-var server = http.createServer(function(req, res) {
+var server = http.createServer(respond);
+
+function respond(req, res) {
   var msg = {ok: true, message: 'Content exist'};
   res.writeHead(200, {'Content-Type': 'application/json'})
   res.end(response.toJson(msg));
-});
+}
 
 server.listen(8000);
